4fa565c0d46c8c29466d386a936443ea70606ae0
ABOXOFJAM <ABOXOFJAM>
2021-05-09 21:58:42 +0800

    2021/5/9 添加闪屏页

---
+++ Cloud_Login/app/src/main/java/com/e/cloud_login/Main_Funcation/SplashActivity.java
@@ -0,0 +1,64 @@
+package com.e.cloud_login.Main_Funcation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.e.cloud_login.R;
+
+public class SplashActivity extends Activity {
+    private LinearLayout linearLayout;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        linearLayout=findViewById(R.id.splash_ll);
+        setAnimation();
+    }
+
+    /**
+     * 设置动画渐变效果
+     */
+    public void setAnimation(){
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.3f,0.1f);
+        alphaAnimation.setDuration(3000);
+        //给布局设置动画效果
+        linearLayout.setAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                   selectActivity();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+    public void selectActivity(){
+        SharedPreferences sharedPreferences = getSharedPreferences("data",MODE_PRIVATE);
+        String First = sharedPreferences.getString("isFirst","0");//设置是否第一次访问
+        //获取isFirst的值,这个值在GuideActivity设置
+        Intent intent =new Intent();
+//        if("0".equals(First)){
+            intent.setClass(this,HomeActivity.class);
+//        }
+//        else{
+//            intent.setClass(this, GuideActivity.class);
+//        }
+        startActivity(intent);
+        finish();
+    }
+}
